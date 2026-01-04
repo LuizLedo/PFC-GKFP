5241377d5edef6ba03c948d9beac17f144538fec
LuizLedo <58785987+LuizLedo@users.noreply.github.com>
2026-01-04 12:40:22 -0300

    Update test_gkfp_iris.R

--- test_gkfp_iris.R
+++ test_gkfp_iris.R
@@ -1,1 +1,15 @@
+rm(list = ls())
+set.seed(123)
+
+source("GKFP1.R")
+
+data(iris)
+X <- as.matrix(iris[, 1:4])
+
+res <- GKFP1(X = X, c = 3, m = 2, zeta = 0.5)
+
+print(res$V)
+print(res$M)
+
+cat("Test completed successfully.\n")
 
