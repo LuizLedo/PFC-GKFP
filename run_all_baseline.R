9bd01ef5509c1d3aca66eaa131def0f6cdea9e58
LuizLedo <58785987+LuizLedo@users.noreply.github.com>
2026-01-26 16:05:27 -0300

    Update run_all_baseline.R

--- run_all_baseline.R
+++ run_all_baseline.R
@@ -1,10 +1,10 @@
 # run_all_baseline.R
 
 # 1) Carrega funções
-source("carregar_dataset.R")      # cria datasets (ou função carregar_datasets())
-source("pca_2pc_prcomp.R")        # sua função PCA
-source("baseline_models.R")       # sua baseline_models()
-source("run_baseline.R")          # sua função run_baseline()
+source("src/carregar_dataset.R")      # cria datasets (ou função carregar_datasets())
+source("src/pca_2pc_prcomp.R")        # sua função PCA
+source("src/baseline_models.R")       # sua baseline_models()
+source("src/run_baseline.R")          # sua função run_baseline()
 
 # 2) Rodar
 out <- run_baseline(datasets, n_repeticoes = 10, seed = 123, usar_pca = TRUE)
